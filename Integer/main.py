b24fe0fb4ea828fb35d7864f903667916049f431
Alisher <alisheryondoshaliyev77@gmail.com>
2025-03-20 14:25:58 +0500

    Yangi fayl Qudrat ustozniki
    Integer masalalarini 24 dan 30 gacha ishlangan

---
+++ Integer/main.py
@@ -0,0 +1,41 @@
+# integer 24
+# k =int(input(" 1 va 365 oralig'ida kun kiriting : "))
+# n = ["yakshanba", "dushanba", "seshanba", "chorshanba", "payshanba", "juma", "shanba"]
+# s = k % 7
+# print(n[s].title())
+
+# integer 25
+# k = int(input("1 va 365 oralig'ida kun kiriting : "))
+# hafta_kumlari = ["chorshanba", "payshanba", "juma", "shanba","yakshanba","dushanba","seshanba"]
+# s = k % 4
+# print(hafta_kumlari[s].title())
+
+# integer 26
+# k = int(input(" Kunni kiriting (1-365): "))
+# hafta_kunlari = ["dushanba", "seshanba", "chorshanba", "payshanba", "juma", "shanba"]
+# kunni_aniqlash = k % 7
+# print(hafta_kunlari[kunni_aniqlash].title())
+
+# integer 27
+# kun = int(input(" Kunni kiriting: "))
+# hafta_kunlari = ["shanba", "yakshanba", "dushanba", "seshanba", "chorshanba", "payshanba", "juma"]
+# kun_tekshirish = kun % 7
+# print(hafta_kunlari[kun_tekshirish].title())
+
+# integer 29
+# a = int(input("to'rtburchakni bir tomoni a = "))
+# b = int(input("to'rtburchakni ikkinchi tomoni b = "))
+# c = int(input("kavadratni tomoni c = "))
+
+# Yuza = a * b
+# kvadrat_yuzasi = c**2
+# kvadrat_sigishi= Yuza // kvadrat_yuzasi
+# ortgan_yuza = Yuza - (kvadrat_sigishi*kvadrat_yuzasi)
+# print(f"Joylashgan kvlar soni {kvadrat_sigishi}")
+# print(f"ortgan yuza {ortgan_yuza}")
+
+# integer 30
+# yil = int(input("Yilni kiriting : "))
+# asr_hisoblash = yil // 100
+# asr = 1 + asr_hisoblash
+# print(f"{yil} yil {asr} asrga kiradi ")
